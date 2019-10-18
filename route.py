4ae1d8bfb98fdb83d9917ef54d06a8771aa22185
teago83 <supremeleaderpastel@gmail.com>
2019-10-18 19:29:47 -0300

    Início do troço do Flask.

--- route.py
+++ route.py
@@ -1,27 +1,40 @@
 from flask import Flask, request, jsonify
+import random
+from aluno import Aluno
 
 app = Flask(__name__)
 
-@app.route("/")
+cassio = Aluno()
+
+@app.route("/") 
 def index():
     return "Hello!"
 
 @app.route("/minhaaplicacao", methods=['GET'])
-def get():
-    return "teste"
+def get(): #cadastrar
+    print("Cadastro do(a) aluno(a):")
+    nome = input("Digite o nome do(a) aluno(a):")
+    sobrenome = input("Digite o sobrenome do(a) aluno(a):")
+    matricula = 100000 + random.randint(1, 99999)
+    print("Matrícula atribuída: %d" % (matricula))
+    cassio.insert(nome, sobrenome, matricula)
+    predo = {
+        "nome" : nome,
+        "sobrenome" : sobrenome,
+        "matrícula" : matricula,
+    }
+    return predo
 
 @app.route("/minhaaplicacao", methods=['POST'])
-def post():
-    data = request.json
-
-    return jsonify(data)
+def post(): #mostrar
+    return "batata"
 
 @app.route("/minhaaplicacao/<int:id>", methods=['PUT'])
-def put(id):
+def put(id): #???
     pass
 
 @app.route("/minhaaplicacao/<int:id>", methods=['DELETE'])
-def delete(id):
+def delete(id): #deletar
     print(id)
 
 
